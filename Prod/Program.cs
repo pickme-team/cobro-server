ada258c2bc334c462ed62113a14ebe6848fde4cb
bakane <dyatlovleonid@gmail.com>
2025-02-28 19:16:32 +0500

    update

--- Prod/Program.cs
+++ Prod/Program.cs
@@ -5,8 +5,6 @@ using Prod.Services;
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
-services.AddOpenApi();
-
 services.AddSwaggerGen();
 
 services.AddDbContext<ProdContext>(o =>
@@ -22,35 +20,12 @@ services.AddDbContext<ProdContext>(o =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
 {
@@ -59,8 +34,3 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
