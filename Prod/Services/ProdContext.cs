9b97c43e627b047c71efb6e02139eb4e098f0146
evgTsv <evgenyscp@gmail.com>
2025-03-01 00:54:36 +0300

    Fix Book

--- Prod/Services/ProdContext.cs
+++ Prod/Services/ProdContext.cs
@@ -6,9 +6,11 @@ namespace Prod.Services;
 public class ProdContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<User> Books { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+        modelBuilder.Entity<Book>().Property(u => u.Status).HasConversion<string>();
     }
 }
