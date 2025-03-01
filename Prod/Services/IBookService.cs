fbef6b870964ffe9fc7cd6b1afe19e5200cc1e8a
evgTsv <evgenyscp@gmail.com>
2025-03-01 09:58:25 +0300

    Start impl book service

--- Prod/Services/IBookService.cs
+++ Prod/Services/IBookService.cs
@@ -4,7 +4,7 @@ public interface IBookService
 {
     Task SetPlaceCount(int count);
     Task<int> PlaceCount();
-    Task BookRoom(Guid roomId, Guid userId, DateTime from, DateTime to);
-    Task BookPlace(Guid placeId, Guid userId, DateTime from, DateTime to);
+    Task BookRoom(Guid roomId, Guid userId, DateTime from, DateTime to, string? description);
+    Task BookPlace(Guid placeId, Guid userId, DateTime from, DateTime to, string? description);
     Task BookSpace(Guid userId, DateTime from, DateTime to);
 }
