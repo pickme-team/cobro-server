f8642e22c17fb6c82265b7e95e8fc4a5bd5a1ce4
bakane <dyatlovleonid@gmail.com>
2025-03-04 01:19:01 +0300

    ADDED GET VERIFICATION PHOTO

--- Prod/Services/IUserService.cs
+++ Prod/Services/IUserService.cs
@@ -7,15 +7,22 @@ namespace Prod.Services;
 public interface IUserService
 {
     Task<UserResponse> UserById(Guid id);
+
     Task<UserResponse> UserByEmail(string email);
+
     Task Update(User user);
+
     Task Delete(Guid id);
 
     // Task UpdateCity(Guid id, string city);
 
     Task<List<UserResponse>> AllUsers();
+
     Task<string> UploadMedia(IFormFile file, Guid id);
+
     Task SetVerificationPhoto(IFormFile file, Guid id);
+    Task<(Stream, string)> GetVerificationPhoto(Guid id);
+
     Task SetPassport(Guid userId, PassportCreateRequest req);
 
     Task<User> Get(Guid id);
