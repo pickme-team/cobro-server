af21d47f29e949818a72bf14af244b6ab06d1e11
Artem Akopian <akopartem1@bk.ru>
2025-03-03 15:37:56 +0300

    test

--- Prod/Models/Database/Request.cs
+++ Prod/Models/Database/Request.cs
@@ -1,13 +1,13 @@
-namespace Prod.Models.Database;
+    namespace Prod.Models.Database;
 
-public class Request
-{
-    public Guid id { get; set; }
-    public string Text { get; set; }
-    public Action Action { get; set; }
-    public long ActionId { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public bool IsRead { get; set; }
-    public Book Book { get; set; }
-    public Guid BookId { get; set; }
-}
+    public class Request
+    {
+        public Guid id { get; set; }
+        public string Text { get; set; }
+        public Action Action { get; set; }
+        public long ActionId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsRead { get; set; }
+        public Book Book { get; set; }
+        public Guid BookId { get; set; }
+    }
